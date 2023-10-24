4a9081b1dcc9fa1e12e2fcf9b3edbabd62f6aa1f
Kamran <70920851+Kam-Ya@users.noreply.github.com>
2023-10-23 23:34:34 -0400

    added dequeue function, edited dequeue_max

--- Assignment2Test/Assignment2Test/main.c
+++ Assignment2Test/Assignment2Test/main.c
@@ -3,7 +3,7 @@
 
 // Node structure
 struct Node {
-    int data;
+    char data;
     int priority;
     struct Node* next;
     struct Node* prev;
@@ -20,7 +20,7 @@ struct Node* initializePriorityQueue() {
 }
 
 // Insert a new node into the priority queue
-void insert(struct Node* sentinel, int data, int priority) {
+void insert(struct Node* sentinel, char data, int priority) {
     // Allocate memory for the new node and set its data and priority
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
@@ -34,17 +34,23 @@ void insert(struct Node* sentinel, int data, int priority) {
 }
 
 // Remove and return the node with the highest priority
-struct Node* dequeue(struct Node* sentinel) {
+char dequeue_max(struct Node* sentinel) {
     if (sentinel->next == sentinel) {
-        return NULL; // Empty queue
+        printf("Queue is empty\n");
+        return 0; // Empty queue
     }
     
+    char info;
+
     // Find the node with the highest priority
     struct Node* maxPriorityNode = sentinel->next;
+    info = maxPriorityNode->data;
     struct Node* temp = sentinel->next->next;
+    
     while (temp != sentinel) {
-        if (temp->priority > maxPriorityNode->priority) {
+        if (temp->priority >= maxPriorityNode->priority) {
             maxPriorityNode = temp;
+            info = maxPriorityNode->data;
         }
         temp = temp->next;
     }
@@ -52,25 +58,51 @@ struct Node* dequeue(struct Node* sentinel) {
     // Remove the node from the list
     maxPriorityNode->prev->next = maxPriorityNode->next;
     maxPriorityNode->next->prev = maxPriorityNode->prev;
-    
-    return maxPriorityNode;
+
+    printf("Succesfully dequeued max\n");
+    free(maxPriorityNode);
+
+    return info;
+}
+
+char dequeue(struct Node* sentinel) {
+    // checks for empty queue
+    if (sentinel->next == sentinel) {
+        printf("Queue is empty\n");
+        return 0;
+    }
+    struct Node* temp = sentinel->next;
+
+    // prio variable to store node values in
+    char info;
+
+    // removes node nearest head from queue while recording its data
+    info = temp->data;
+    temp->prev->next = temp->next;
+    temp->next->prev = temp->prev;
+
+    // frees removed node and prints success statement
+    free(temp);
+    printf("succesfully dequeued\n");
+
+    return info;
 }
 
 int main() {
     struct Node* sentinel = initializePriorityQueue();
     
-    insert(sentinel, 1, 3);
-    insert(sentinel, 2, 1);
-    insert(sentinel, 3, 4);
-    insert(sentinel, 4, 2);
+    insert(sentinel, '1', 3);
+    insert(sentinel, '2', 1);
+    insert(sentinel, '3', 4);
+    insert(sentinel, '4', 2);
+    insert(sentinel, '7', 4);
     
-    struct Node* maxNode = dequeue(sentinel);
-    while (maxNode != NULL) {
-        printf("Data: %d, Priority: %d\n", maxNode->data, maxNode->priority);
-        free(maxNode);
-        maxNode = dequeue(sentinel);
+    printf("Data: %c\n", dequeue_max(sentinel));
+
+    while (sentinel->next != sentinel) {
+        printf("Data: %c\n", dequeue(sentinel));
     }
-    
+
     // Free the sentinel node
     free(sentinel);
     
